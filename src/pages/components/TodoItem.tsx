a824430c4e9ca4b62ff476293a0049ec1fc089f3
Daffadon <daffaputranarendra9@gmail.com>
2023-04-07 19:00:46 +0700

    add done section and done handler

--- src/pages/components/TodoItem.tsx
+++ src/pages/components/TodoItem.tsx
@@ -2,31 +2,56 @@ import React from "react";
 import { Todo } from "../index";
 import { AiFillDelete } from "react-icons/ai"
 import { BsCheckLg, BsPencilSquare } from "react-icons/bs"
+import { MdRestore } from "react-icons/md"
 interface TodoItemProps {
   todo: Todo
   todos: Todo[]
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+  done: Todo[]
+  setDone: React.Dispatch<React.SetStateAction<Todo[]>>
+  isDone: boolean
 }
-const TodoItem: React.FC<TodoItemProps> = ({ todo, todos, setTodos }) => {
+const TodoItem: React.FC<TodoItemProps> = ({ todo, todos, setTodos, done, setDone, isDone }) => {
   const deleteHandler = (itemToDelete: Todo) => {
-    const newTodos=todos.filter((item)=>{
-      return item.id !== itemToDelete.id
-    })
-    setTodos(newTodos);
+    if (!isDone) {
+      const newTodos = todos.filter((item) => {
+        return item.id !== itemToDelete.id
+      })
+      setTodos(newTodos);
+    } else {
+      const newDone = done.filter((item) => {
+        return item.id !== itemToDelete.id
+      })
+      setDone(newDone)
+    }
+  }
+  const doneHandler = (itemToBeDone: Todo) => {
+    setDone(prev => ([...prev, itemToBeDone]))
+    deleteHandler(itemToBeDone);
   }
   return (
     <div className="bg-orange-300 rounded-xl flex items-center justify-between px-3 py-4">
-      <p className="font-bold ">{todo.name}</p>
+      <p className="font-bold text-xl">{todo.name}</p>
       <div className="flex gap-2">
         <button onClick={deleteHandler.bind(this, todo)}>
           <AiFillDelete size={35} className="hover:text-red-500 transition-all duration-300" />
         </button>
-        <button>
-          <BsPencilSquare size={35} className="hover:text-blue-700 transition-all duration-300" />
-        </button>
-        <button>
-          <BsCheckLg size={35} className="hover:text-green-600 transition-all duration-300" />
-        </button>
+        {!isDone ?
+          <>
+            <button>
+              <BsPencilSquare size={35} className="hover:text-blue-700 transition-all duration-300" />
+            </button>
+            <button onClick={doneHandler.bind(this, todo)}>
+              <BsCheckLg size={35} className="hover:text-green-600 transition-all duration-300" />
+            </button>
+          </>
+          :
+          <>
+            <button>
+              <MdRestore size={35} className="hover:text-blue-700 transition-all duration-300" />
+            </button>
+          </>
+        }
       </div>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputField from "./components/InputField";
 import TodoItem from "./components/TodoItem";
 
 export interface Todo {
   id: number;
   name: string;
-  isDone: boolean;
 }
 
 export default function Home(): JSX.Element {
   const [activity, setActivity] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [done, setDone] = useState<Todo[]>([]);
   const addHandler = (e: React.FormEvent) => {
     e.preventDefault();
     if (activity) {
-      setTodos([...todos, { id: Date.now(), name: activity, isDone: false }]);
+      setTodos([...todos, { id: Date.now(), name: activity }]);
       setActivity("");
     }
   };
+  useEffect(() => {
+    console.log(done)
+  })
   return (
     <>
       <main>
         <div className=" h-[12vh] grid place-content-center bg-stone-900 ">
-          <p className=" font-bold text-2xl text-white">To do List</p>
+          <p className=" font-bold text-2xl text-white">To Do List</p>
         </div>
         <div >
           <InputField
@@ -30,10 +33,19 @@ export default function Home(): JSX.Element {
             setActivity={setActivity}
             addHandler={addHandler} />
         </div>
+        <p className="text-center font-extrabold bg-yellow-500 py-3 text-white text-3xl mt-5">TO DO</p>
         <div className="flex justify-center items-center">
           <div className="mt-5 w-10/12 grid place-content-center gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
             {todos.map(todo => {
-              return <TodoItem todo={todo} todos={todos} setTodos={setTodos} key={todo.id} />
+              return <TodoItem todo={todo} todos={todos} setTodos={setTodos} done={done} setDone={setDone} isDone={false} key={todo.id} />
+            })}
+          </div>
+        </div>
+        <p className="text-center font-extrabold bg-green-500 py-3 mt-10 text-white text-3xl">DONE</p>
+        <div className="flex justify-center items-center">
+          <div className="mt-5 w-10/12 grid place-content-center gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
+            {done.map(todo => {
+              return <TodoItem todo={todo} todos={todos} setTodos={setTodos} done={done} setDone={setDone} isDone={true} key={todo.id} />
             })}
           </div>
         </div>
